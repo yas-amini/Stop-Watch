f9472100ca36a279a48824bc6f22493f953b974a
Yasamin <yaasamin.amini@gmail.com>
2024-08-19 17:32:40 +0200

    More JS

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ let secondsElapsed = 0;
 let interval = null; 
 const time = document.getElementById("time")
 
+function setTime(){
+    const minutes = Math.floor(secondsElapsed/ 60)
+    const seconds = secondsElapsed % 60
+    time.innerHTML = `${minutes}: ${seconds}`;
+}
 function timer(){
     secondsElapsed++;
+    setTime()
 }
 
 function startClock(){
