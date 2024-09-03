ad7b24181d012805e688b588b8932628771aba00
Yasamin <yaasamin.amini@gmail.com>
2024-09-03 11:06:55 +0200

    Fixed both files

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 let secondsElapsed = 0;
-let interval = null; 
+let interval = null;
 const time = document.getElementById("time")
 
-function padStart(value){
+function padStart(value) {
     return String(value).padStart(2, "0")
 }
-function setTime(){
-    const minutes = Math.floor(secondsElapsed/ 60)
+
+function setTime() {
+    const minutes = Math.floor(secondsElapsed / 60)
     const seconds = secondsElapsed % 60
-    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}`;
+    time.innerHTML = `${padStart(minutes)}:${padStart(seconds)}` ;
 }
-function timer(){
+
+function timer() {
     secondsElapsed++;
     setTime()
 }
 
-function startClock(){
+function startClock() {
     if (interval) stopClock()
     interval = setInterval(timer, 1000)
 }
 
-function stopClock(){
+function stopClock() {
     clearInterval(interval)
 }
 
-function resetClock(){
+function resetClock() {
     stopClock()
     secondsElapsed = 0;
     setTime()
